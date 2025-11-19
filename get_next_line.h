3d982f3ef80bc2db381910367b6d533db8c98d45
Kent1C38 <corsiusquentin@gmail.com>
2025-11-19 15:43:08 +0100

    Completed GNL function

--- get_next_line.h
+++ get_next_line.h
@@ -6,24 +6,28 @@
 /*   By: qcorsius <qcorsius@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 10:23:09 by qcorsius          #+#    #+#             */
-/*   Updated: 2025/11/17 10:31:06 by qcorsius         ###   ########lyon.fr   */
+/*   Updated: 2025/11/18 16:22:45 by qcorsius         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 # include <stdlib.h>
-# include <unistd.h>
 
 typedef struct s_entry
 {
-	int	index;
-	int	fd;
+	int				fd;
+	char			*save;
+	struct s_entry	*next;
 }	t_entry;
 
-typedef struct s_repertory
-{
-	t_entry		entry;
-	t_repertory	next;
-}	t_repertory;
+char	*get_next_line(int fd);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+t_entry	*get_entry(t_entry *rep, const int fd);
+void	append_entry(t_entry **entry, t_entry *new_entry);
+t_entry	*new_entry(int fd);
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,20 +6,79 @@
 /*   By: qcorsius <qcorsius@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 10:25:56 by qcorsius          #+#    #+#             */
-/*   Updated: 2025/11/17 16:31:13 by qcorsius         ###   ########lyon.fr   */
+/*   Updated: 2025/11/18 16:22:24 by qcorsius         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
 
-t_entry *get(t_repertory *rep, int index)
+void	ft_bzero(void *s, size_t n)
 {
-	t_repertory	*tmp;
+	size_t			i;
+	unsigned char	*p;
+
+	if (s == NULL)
+		return ;
+	i = 0;
+	p = s;
+	while (i < n)
+	{
+		p[i] = (unsigned char) '\0';
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*array;
+
+	if (!size || !nmemb)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array != NULL)
+		ft_bzero(array, nmemb * size);
+	return (array);
+}
+
+t_entry	*new_entry(int fd)
+{
+	t_entry	*entry;
+
+	entry = malloc(sizeof(t_entry));
+	if (entry == NULL)
+		return (NULL);
+	entry->fd = fd;
+	entry->save = ft_calloc(1, sizeof(char));
+	if (entry->save == NULL)
+	{
+		free(entry);
+		return (NULL);
+	}
+	entry->next = NULL;
+	return (entry);
+}
+
+void	append_entry(t_entry **entry, t_entry *new_entry)
+{
+	t_entry	*tmp;
+
+	tmp = *entry;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new_entry;
+}
+
+t_entry	*get_entry(t_entry *rep, const int fd)
+{
+	t_entry	*tmp;
 
 	tmp = rep;
-	while (tmp)
+	while (tmp != NULL)
 	{
-		
+		if (tmp->fd == fd)
+			return (tmp);
 		tmp = tmp->next;
 	}
+	return (NULL);
 }
